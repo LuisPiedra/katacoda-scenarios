0feb1c9c79002df54130353d72efa7185f52e75d
Luis Piedra <luis.piedra@ebury.com>
2022-01-07 11:25:39 +0100

    no auto start

--- test/set-env.sh
+++
@@ -1,4 +0,0 @@
-sleep 1
-echo "Starting Kubernetes 1.15.0. This may take a few moments, please wait..."
-while [ `minikube status &>/dev/null; echo $?` -ne 0 ]; do sleep 1; done
-echo "Kubernetes Started"
